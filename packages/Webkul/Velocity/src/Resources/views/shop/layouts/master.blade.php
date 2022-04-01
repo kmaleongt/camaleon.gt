f0f5e19c60e5076b4ba3e0c199227ba30c3b1df9
Henry Díaz <hdiaz@realsoft.dev>
2022-04-01 12:16:15 -0600

    Se mejoro el lugar del chat de facebook

--- packages/Webkul/Velocity/src/Resources/views/shop/layouts/master.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/layouts/master.blade.php
@@ -64,42 +64,15 @@
     </head>
 
     <body @if (core()->getCurrentLocale()->direction == 'rtl') class="rtl" @endif>
-
-        {!! view_render_event('bagisto.shop.layout.body.before') !!}
-
-        @include('shop::UI.particals')
-
-        <!-- Messenger Plugin de chat Code -->
-
-        <script>
-        var chatbox = document.getElementById('fb-customer-chat');
-        chatbox.setAttribute("page_id", "105553294705491");
-        chatbox.setAttribute("attribution", "biz_inbox");
-        </script>
-
-        <!-- Your SDK code -->
-        <script>
-        window.fbAsyncInit = function() {
-            FB.init({
-            xfbml            : true,
-            version          : 'v13.0'
-            });
-        };
-
-        (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-        </script>
-
         <div id="fb-root"></div>
 
         <!-- Your Plugin de chat code -->
         <div id="fb-customer-chat" class="fb-customerchat">
         </div>
+        {!! view_render_event('bagisto.shop.layout.body.before') !!}
+
+        @include('shop::UI.particals')
+
 
         <div id="app">
             {{-- <responsive-sidebar v-html="responsiveSidebarTemplate"></responsive-sidebar> --}}
